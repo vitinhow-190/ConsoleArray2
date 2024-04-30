158212d6d87f0cde1a7792fb242338742c730eef
Usuario <Usuario@CPD02-M07>
2024-04-30 11:11:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleArray2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArray2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = new string[3];
+
+            Console.WriteLine("Digite o primeiro nome: ");
+            nome[0] = Console.ReadLine();
+            Console.WriteLine("Digite o segundo nome: ");
+            nome[1] = Console.ReadLine();
+            Console.WriteLine("digite o terceiro nome: ");
+            nome[2] = Console.ReadLine();
+
+            Console.WriteLine("Primeiro nome: " + nome[0]);
+            Console.WriteLine("Segundo nome: " + nome[1]);
+            Console.WriteLine("Terceiro nome: " + nome[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
